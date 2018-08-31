89ea099293bc8434655cbcf90cdca45ef79242a5
wenbin <a030950222@163.com>
2018-08-31 16:59:20 +0800

    modify sample 3

--- sample.cpp
+++ sample.cpp
@@ -3,6 +3,6 @@
 int main()
 {
 	std::cout<<"hello github!"<<std::endl;
-        std::cout<<"I eat an apple.It is delicious"<<endl;        
+        std::cout<<"I eat an apple.It is very delicious"<<endl;        
         return 0;
 }
