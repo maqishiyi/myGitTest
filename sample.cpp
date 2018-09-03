0a68baeceb785ffd731fed2a34af725192deb8e2
wenbin <a030950222@163.com>
2018-09-03 10:17:27 +0800

    branch test

--- sample.cpp
+++ sample.cpp
@@ -5,6 +5,7 @@ int main()
 	std::cout<<"hello github!"<<std::endl;
         std::cout<<"I eat an apple.It is very delicious"<<std::endl;        
 	std::cout<<"I build a new branch"<<std::endl;
-	std::cout<<"I build a new branch v1.2"<<std::endl;       
+	std::cout<<"I build a new branch v1.2"<<std::endl;
+	std::cout<<"branch test"<<std::endl;       
 	return 0;
 }
