8db9a55082f272c2051dec52afdaddc3ea29a060
wenbin <a030950222@163.com>
2018-09-03 10:14:43 +0800

    build a new branch 1.2

--- sample.cpp
+++ sample.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout<<"hello github!"<<std::endl;
         std::cout<<"I eat an apple.It is very delicious"<<std::endl;        
-	std::cout<<"I build a new branch"<<std::endl;       
+	std::cout<<"I build a new branch"<<std::endl;
+	std::cout<<"I build a new branch v1.2"<<std::endl;       
 	return 0;
 }
