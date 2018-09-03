5ceb10b79e439de2dfc79e842afbb6c656edcdf2
wenbin <a030950222@163.com>
2018-09-03 10:36:03 +0800

    pull test 2

--- sample.cpp
+++ sample.cpp
@@ -6,6 +6,6 @@ int main()
         std::cout<<"I eat an apple.It is very delicious"<<std::endl;        
 	std::cout<<"I build a new branch"<<std::endl;
 	std::cout<<"I build a new branch v1.2"<<std::endl;
-	std::cout<<"branch test"<<std::endl;       
+	std::cout<<"branch test 2"<<std::endl;       
 	return 0;
 }
